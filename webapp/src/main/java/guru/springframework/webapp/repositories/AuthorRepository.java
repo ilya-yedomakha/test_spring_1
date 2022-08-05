8bfe66563a4acd98a727475030cc749715a100c8
ilya-yedomakha <yedomakhailia@gmail.com>
2022-08-05 12:47:56 +0300

    newCommit6

---
+++ webapp/src/main/java/guru/springframework/webapp/repositories/AuthorRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.webapp.repositories;
+
+import guru.springframework.webapp.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author,Long>{
+
+}
